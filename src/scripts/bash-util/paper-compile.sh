01967a6f96f133c4f7596deb7d64ac69949e4d98
papum <danieledugo1@gmail.com>
2026-05-12 13:21:51 +0200

    compile paper

--- src/scripts/bash-util/paper-compile.sh
+++ src/scripts/bash-util/paper-compile.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# Check for --save flag
+SAVE_PDF=false
+if [[ "$*" == *"--save"* ]]; then
+    SAVE_PDF=true
+    echo "Will save PDF to docs/paper/paper.pdf after compilation"
+fi
+
+# Find docs/paper/ absolute path before cd
+DOCS_PAPER_PATH=$(find . -type d -path "*/docs/paper" 2>/dev/null | head -n 1)
+if [ -n "$DOCS_PAPER_PATH" ]; then
+    DOCS_PAPER_ABS=$(cd "$DOCS_PAPER_PATH" && pwd)
+else
+    echo "Warning: Could not find docs/paper/ directory"
+    SAVE_PDF=false
+    echo "Will not save PDF after compilation"
+fi
+
 # Use first argument if provided, otherwise default to src/paper1/
 PAPER_DIR="${1:-src/paper1/}"
 
 if [ -d "$PAPER_DIR" ]; then
-	cd "$PAPER_DIR"
+    cd "$PAPER_DIR"
 else
     echo "Error: Directory $PAPER_DIR does not exist"
     exit 1
@@ -16,3 +33,9 @@ pdflatex main.tex
 bibtex main
 pdflatex main.tex
 pdflatex main.tex
+
+# Copy to docs/paper if --save flag was set
+if [ "$SAVE_PDF" = true ] && [ -f main.pdf ]; then
+    cp main.pdf "$DOCS_PAPER_ABS/paper.pdf"
+    echo "Saved PDF to $DOCS_PAPER_ABS/paper.pdf"
+fi
